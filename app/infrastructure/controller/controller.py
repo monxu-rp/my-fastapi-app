c5b72d430587bc1f96b89f116ebda172e996608d
Ramon Puiggene Brecha <ramon.puiggene@real-digital.de>
2025-05-10 23:09:38 +0200

    Define a CountryResponse to be used by fastapi

--- app/infrastructure/controller/controller.py
+++ app/infrastructure/controller/controller.py
@@ -7,21 +7,25 @@ from app.domain.exceptions.exceptions import CountryNotFoundException
 from app.domain.ports.country_service import CountryService
 from app.domain.use_cases.get_country_name import GetCountryName
 from app.infrastructure.client.rest_country_client import RestCountryClient
+from pydantic import BaseModel
+
+class CountryResponse(BaseModel):
+    country: str
 
 router = APIRouter()
 
 def get_country_service() -> CountryService:
     return RestCountryClient()
 
-@router.get("/country/{code}")
+@router.get("/country/{code}", response_model=CountryResponse)
 async def get_country_name(
         code: str = Path(..., pattern="^[a-zA-Z]{2,3}$"),
         country_service: CountryService = Depends(get_country_service)
-) -> Dict[str, str]:
+) -> CountryResponse:
     use_case = GetCountryName(country_service)
     try:
         country_name = await use_case.execute(code)
-        return {"country": country_name}
+        return CountryResponse(country=country_name)
     except CountryNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
